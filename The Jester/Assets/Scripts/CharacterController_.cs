e563a329cb67125937f8535df06c83ab60a831a6
Yunus Emre ASLAN <40874281+Dubium0@users.noreply.github.com>
2024-01-28 13:38:55 +0300

    Update CharacterController_.cs

--- The Jester/Assets/Scripts/CharacterController_.cs
+++ The Jester/Assets/Scripts/CharacterController_.cs
@@ -2,8 +2,7 @@ using Cinemachine;
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
+
 using UnityEngine;
 
 public class CharacterController_ : MonoBehaviour
