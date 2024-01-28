c55d7650b4f0793f5e74b87331513de3f8b9b38f
OnurOzdemir0 <onur.ozdemir@ozu.edu.tr>
2024-01-28 15:25:52 +0300

    sounds added

---
+++ The Jester/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager soundManager;
+
+    [SerializeField] private AudioClip[] toparlicamClips;
+    [SerializeField] private AudioClip kusuraBakmayinClip; // Play when one health is left
+    [SerializeField] private AudioClip[] kingLaughClips;
+    [SerializeField] private AudioClip kingLaughDeathClip;
+    [SerializeField] private AudioClip cardPullClip;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (soundManager != null && soundManager != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            soundManager = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void PlayRandomToparlicamSound()
+    {
+        PlayRandomSound(toparlicamClips);
+    }
+
+    public void PlayKusuraBakmayinSound()
+    {
+        audioSource.PlayOneShot(kusuraBakmayinClip);
+    }
+
+    public void PlayRandomKingLaughSound()
+    {
+        PlayRandomSound(kingLaughClips);
+    }
+
+    public void PlayKingLaughDeathSound()
+    {
+        audioSource.PlayOneShot(kingLaughDeathClip);
+    }
+
+    public void PlayCardPullSound()
+    {
+        audioSource.PlayOneShot(cardPullClip);
+    }
+
+    private void PlayRandomSound(AudioClip[] clips)
+    {
+        if (clips.Length == 0) return;
+
+        int index = Random.Range(0, clips.Length);
+        audioSource.PlayOneShot(clips[index]);
+    }
+}
